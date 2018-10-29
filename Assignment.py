f15a08483acaecfc548738326c1d3d21af4cabff
CANicholson <canicholson96@gmail.com>
2018-10-29 15:09:48 +0000

    Added in file parsing to go through each line. Added capitalisation function. Added string cleaning that removes apostrephes.

--- Assignment.py
+++ Assignment.py
@@ -3,6 +3,12 @@ def main(filename):
 
     The lower the score the better the acronym. The acronyms are then stored in an output file"""
     if filename.endswith(".txt"):
-        pass
+        with open(filename, 'r') as filestrings, open("names_abbrevs.txt", 'w') as fileabrevs:
+            fullstring = filestrings.readline()
+            while(fullstring!=""):
+                fullstring = fullstring.upper()
+                cleanedstring = fullstring.replace("'", "")
+                print(cleanedstring)
+                fullstring = filestrings.readline()
     else: 
         print("File is not a .txt. Please try again with a .txt file")
